abbcccf18f1f76ed54251fdd96e07a41be0e446c
Andre <andre@kaelo.agency>
2017-09-19 22:27:07 -0600

    updated register component

---
+++ client/src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="register">
+    <h1>Register here:</h1>
+    <br />
+    <input
+      type="email"
+      name="email"
+      placeholder="email..."
+      v-model="email"
+    />
+    <br />
+    <input
+      type="password"
+      name="password"
+      placeholder="your password..."
+      v-model="password"
+    />
+    <br />
+    <button
+      @click="newRegister"
+    >Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async newRegister() {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
